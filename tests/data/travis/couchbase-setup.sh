f830b23999ba91ce0bdb804b644640dffc6cb2ba
romeOz <serggalka@gmail.com>
2015-11-07 23:47:27 +0300

    Added packages libcouchbase-dev and libcouchbase2-libevent to travis.

--- tests/data/travis/couchbase-setup.sh
+++ tests/data/travis/couchbase-setup.sh
@@ -6,33 +6,34 @@ if (php --version | grep -i HipHop > /dev/null); then
 fi
 
 # Download and uncompress Couchbase Server
+#http://packages.couchbase.com/releases/4.0.0/couchbase-server-community_4.0.0-ubuntu12.04_amd64.deb
 wget http://packages.couchbase.com/releases/3.0.1/couchbase-server-community_3.0.1-ubuntu12.04_amd64.deb
 sudo dpkg -i couchbase-server-community_3.0.1-ubuntu12.04_amd64.deb
 
-# Add couchbase.list to sources.list
-sudo wget -O/etc/apt/sources.list.d/couchbase.list http://packages.couchbase.com/ubuntu/couchbase-ubuntu1204.list
-sudo wget -O- http://packages.couchbase.com/ubuntu/couchbase.key | sudo apt-key add -
-
-# update list
-sudo apt-get update
-
-# install C Client Library
-sudo apt-get install libcouchbase2-libevent
-sudo apt-get install libcouchbase-dev
-
-# this is helpful to compile extension
-sudo apt-get install autoconf
+## Add couchbase.list to sources.list
+#sudo wget -O/etc/apt/sources.list.d/couchbase.list http://packages.couchbase.com/ubuntu/couchbase-ubuntu1204.list
+#sudo wget -O- http://packages.couchbase.com/ubuntu/couchbase.key | sudo apt-key add -
+#
+## update list
+#sudo apt-get update
+#
+## install C Client Library
+#sudo apt-get install libcouchbase2-libevent
+#sudo apt-get install libcouchbase-dev
+#
+## this is helpful to compile extension
+#sudo apt-get install autoconf
 
 # install this version
 VERSION=2.1.0
 
 #cd /tmp/
-
-# compile manually, because `pecl install` keep asking questions
-wget http://pecl.php.net/get/couchbase-${VERSION}.tgz
-tar zxvf couchbase-${VERSION}.tgz
-cd "couchbase-${VERSION}"
-phpize && ./configure && make install && echo "Installed ext/couchbase-${VERSION}"
+pecl install -f couchbase-${VERSION}
+## compile manually, because `pecl install` keep asking questions
+#wget http://pecl.php.net/get/couchbase-${VERSION}.tgz
+#tar zxvf couchbase-${VERSION}.tgz
+#cd "couchbase-${VERSION}"
+#phpize && ./configure && make install && echo "Installed ext/couchbase-${VERSION}"
 
 #echo "extension = couchbase.so" >> /etc/php5/fpm/conf.d/couchbase.ini
 #echo "extension = couchbase.so" >> /etc/php5/cli/conf.d/couchbase.ini
