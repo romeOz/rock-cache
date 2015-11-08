aaee78c474af0f7caad2068f5cb60143f7130a1c
romeOz <serggalka@gmail.com>
2015-11-08 06:02:23 +0300

    Create user mongo to travis.

--- tests/data/travis/mongodb-setup.sh
+++ tests/data/travis/mongodb-setup.sh
@@ -2,20 +2,23 @@
 #
 # install mongodb
 
-if (php --version | grep -i HipHop > /dev/null); then
-  echo "mongodb does not work on HHVM currently, skipping"
-  exit 0
-else
-  echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-fi
+docker exec -it mongodb mongo rocktest --eval 'db.createUser({user: "travis", pwd: "test", roles:[{role:"root",db:"admin"}]})'
 
-# show mongodb version
-mongod --version
 
-# show mongo PHP extension version
-php -i |grep mongo -4 |grep -2 Version
-
-# enable text search
-mongo --eval 'db.adminCommand( { setParameter: true, textSearchEnabled : true})'
-
-cat /etc/mongodb.conf
+#if (php --version | grep -i HipHop > /dev/null); then
+#  echo "mongodb does not work on HHVM currently, skipping"
+#  exit 0
+#else
+#  echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+#fi
+#
+## show mongodb version
+#mongod --version
+#
+## show mongo PHP extension version
+#php -i |grep mongo -4 |grep -2 Version
+#
+## enable text search
+#mongo --eval 'db.adminCommand( { setParameter: true, textSearchEnabled : true})'
+#
+#cat /etc/mongodb.conf
